3ab6cbd843d5a6710114b5783511191f5a00f166
Ismael Rosende Rey <isrosrey@uoc.edu>
2020-11-07 20:03:02 +0100

    Corregir salida CSV y hacer pruebas sección y categoría

--- src/PriceTracker/spiders/carrefour_spider.py
+++ src/PriceTracker/spiders/carrefour_spider.py
@@ -1,5 +1,6 @@
 import datetime
 import pandas as pd
+import pathlib
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from ..items import PricetrackerItem
@@ -9,20 +10,19 @@ class CarrefourSpider(scrapy.Spider):
     fecha = '{0:%d%m%Y_%H%M}'.format(datetime.datetime.now())
     fileName = 'CarrefourDailyPricing_' + fecha + '.csv'
     filePath = '../CSVdata/' + fileName
-    custom_settings = {'FEED_URI': filePath, 'FEED_FORMAT': 'csv'}
-    
-    # custom_settings = {pathlib.Path(filePath): {'format': 'csv',
-    #                                             'encoding': 'utf8-sig',
-    #                                             'store_empty': True,
-    #                                             'fields': ['seccion',
-    #                                                        'categoria',
-    #                                                        'descripcion',
-    #                                                        'precio_Kg_L_ud',
-    #                                                        'precio',
-    #                                                        'precioPrevio',
-    #                                                        'precioOferta',
-    #                                                        'promocion',
-    #                                                        'enlace'],},}
+    # custom_settings = {'FEED_URI': filePath, 'FEED_FORMAT': 'csv'}
+    custom_settings = {'FEEDS': {pathlib.Path(filePath): {'format': 'csv',
+                                                'encoding': 'utf8',
+                                                'store_empty': True,
+                                                'fields': ['seccion',
+                                                           'categoria',
+                                                           'descripcion',
+                                                           'precio_Kg_L_ud',
+                                                           'precio',
+                                                           'precioPrevio',
+                                                           'precioOferta',
+                                                           'promocion',
+                                                           'enlace'],}}}
 
     def start_requests(self):
         urls=['https://www.carrefour.es/supermercado/']
@@ -42,10 +42,9 @@ class CarrefourSpider(scrapy.Spider):
         listaSecciones = response.css('li.level2-item')
         # print("Se han encontrado {} secciones.".format(len(listaSecciones)))
         # Para cada sección, seguimos el enlace hacia las diferentes categorías
-        for seccion in listaSecciones:
-            item = PricetrackerItem()
+        for seccion in listaSecciones[0:2]:
             # Se captura el nombre de la sección
-            item['seccion'] = seccion.css('a ::text').extract_first()
+            nombreSeccion = seccion.css('a ::text').extract_first()
             # print("Seccion: {}".format(nombreSeccion))
             # Se extra el link de esta sección
             link = seccion.css('a::attr(href)').extract_first()
@@ -54,16 +53,14 @@ class CarrefourSpider(scrapy.Spider):
             # print("Seccion siguiente: {}".format(next_page_url))
             yield response.follow(url=next_page_url,
                                   callback=self.parse_categoria,
-                                  meta={'item':item})
+                                  cb_kwargs={'seccion':nombreSeccion})
 
-    def parse_categoria(self, response):
+    def parse_categoria(self, response, seccion):
         # Extraemos el código html de las diferentes categorias
         listaCategorias = response.css('div.category')
         # print("Se han encontrado {} categorias.".format(len(listaCategorias)))
-        for categoria in listaCategorias:
-            # Se recupera el item generado en el parse anterior
-            item = response.meta['item']
-            item['categoria'] = categoria.css('p.nombre-categoria::text')\
+        for categoria in listaCategorias[0:2]:
+            nombreCategoria = categoria.css('p.nombre-categoria::text')\
                 .extract_first()
             # print("Categoría: {}".format(nombreCategoria))
             # Se extrae el link de esta categoria
@@ -73,22 +70,24 @@ class CarrefourSpider(scrapy.Spider):
             # print("Categoria siguiente: {}".format(next_page_url))
             yield response.follow(url=next_page_url,
                                   callback=self.parse_productos,
-                                  meta={'item':item})
+                                  cb_kwargs={'seccion':seccion,
+                                             'categoria': nombreCategoria})
 
-    def parse_productos(self, response):
+    def parse_productos(self, response, seccion, categoria):
         # Se filtran los "product-card-item" para no mezclar precios
         items_producto = response.css('article.product-card-item')
         # print("Se han encontrado {} productos".format(len(items_producto)-1))
         # Se recorre cada item para extraer el nombre, los precios y las
         # ofertas
-        for producto in items_producto:
+        for producto in items_producto[0:2]:
             descripcion = producto.css('p.title-product ::text')\
                 .extract_first()
             # Si hay descripción asignamos los valores, sino se comprueba si
             # hay página siguiente
             if(descripcion):
-                # Se recupera el item generado en el parse anterior
-                item = response.meta['item']
+                item = PricetrackerItem()
+                item['seccion']=seccion
+                item['categoria']=categoria
                 # Buscamos los nombres del producto
                 item['descripcion'] = descripcion
                 item['precio'] = producto.css('span.price ::text')\
